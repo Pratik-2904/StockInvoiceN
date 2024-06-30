addab681215a15df5a7f3d0a09a52d6733e05c23
Pratik <pratiksukale2904@gmail.com>
2024-06-30 12:34:23 +0530

    new commit

---
+++ app/src/main/java/com/example/stockinvoice/homeScreen/HomeView.kt
@@ -0,0 +1,18 @@
+package com.example.stockinvoice.homeScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+class HomeView {
+}
+@Composable
+fun homeView(){
+
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun homeViewPreview(){
+    homeView()
+
+}
